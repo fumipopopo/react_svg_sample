c1aecb059790b2e1af0509f83d201c94b968248e
fumipo <46805293+fumipopopo@users.noreply.github.com>
2023-08-24 04:26:53 +0000

    不要なお試しコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ import Timer from "./components/Timer";
 import { useState } from "react";
 
 export default function App() {
-  const [positionSVG, setPositionSVG] = useState({ x: 0, y: 0 });
-
-  const handleMouseMove = (e: React.MouseEvent<SVGSVGElement>) => {
-    const newX = e.clientX;
-    const newY = e.clientY;
-    setPositionSVG({ x: newX, y: newY });
-  };
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
